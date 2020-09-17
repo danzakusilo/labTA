3144228d04c903fd4b43b3a70b30f55d4ea6f380
danzakusilo <60900104+EchoAstral@users.noreply.github.com>
2020-09-17 21:29:32 +0300

    Insertion and selection sorting algorithms comparison

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
-if __name__ == '__main__':
+# Вариант 8
 
-    array = []
+
+def selection_sort(arr):
+    amount = 0
+    length = len(arr)
+    # set i as amount of elements in the array
+    for i in range(length):
+        # set lowest_value_index as current i
+        lowest_value_index = i
+        # for remaining unsorted elements
+        for j in range(i+1, length):
+            if arr[j] < arr[lowest_value_index]:
+                lowest_value_index = j
+            arr[i], arr[lowest_value_index] = arr[lowest_value_index], arr[i]
+            amount += 1
+    return amount
+
+
+def insertion_sort(arr):
+    length = len(arr)
+    amount = 0
+    # assume first elem is already sorted
+    for i in range(1, length):
+        item_to_insert = arr[i]
+        # link to previous elem
+        j = i - 1
+        # move elems up front if elem > item_to_insert
+        while j >= 0 and arr[j] < item_to_insert:
+            amount += 1
+            arr[j + 1] = arr[j]
+            j -= 1
+        # inserting elem
+        arr[j + 1] = item_to_insert
+    return amount
+
+
+if __name__ == '__main__':
+    array = [61, 32, 27, 45, 75, 58, 5, 50, 99]
+    # 36 exchanges. This algorithm has complexity of n^2 so it will complete 9^2=81 exchanges in the worst case
+    print("Overall %d exchanges" % selection_sort(array))
+    print("Sorted array", array) # [5, 27, 32, 45, 50, 58, 61, 75, 99]
+
+    # shuffling array again
+    array = [61, 32, 27, 45, 75, 58, 5, 50, 99]
+    # 21 exchanges. This algorithm has complexity of n^2 so it will complete 9^2=81 exchanges in the worst case
+    print("Overall %d exchanges" % insertion_sort(array))
+    print("Sorted array", array)   # [5, 27, 32, 45, 50, 58, 61, 75, 99]
+
+
+
+
+
